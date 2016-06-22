f58a384cb905d0125e18ae7efc7d314211537a21
RohanM14 <rohanmuthukumar@gmail.com>
2016-06-22 16:35:42 -0400

    Assignment 1

---
+++ Assignment1/Assignment1/InstrumentationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Assignment1
+//
+//  Created by Rohan Muthukumar on 6/20/16.
+//  Copyright © 2016 Rohan Muthukumar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
