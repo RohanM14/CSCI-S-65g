1e0ed4cb7a9a8deb1eac7a662c720cae6077fbbc
RohanM14 <rohanmuthukumar@gmail.com>
2016-06-29 16:56:06 -0400

    Assignment 2

---
+++ Assignment2/Assignment2/MainController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Rohan Muthukumar on 6/29/16.
+//  Copyright © 2016 Rohan Muthukumar. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
